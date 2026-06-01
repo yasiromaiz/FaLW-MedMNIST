ca0ba0d4e08c404b1a82f7e2c8d78b866bea7452
yasiromaiz <yasiromaiz786@gmail.com>
2026-06-01 07:01:09 +0530

    We updated the __init__.py

--- evaluation/__init__.py
+++ evaluation/__init__.py
@@ -1,3 +1,6 @@
 from .MIA import MIA
 from .SVC_MIA import SVC_MIA
-from .MyMIA import get_membership_attack_prob
+
+# from .MyMIA import get_membership_attack_prob
+
+#NOTE: The above line is commented because the file it wants to extract is absent
