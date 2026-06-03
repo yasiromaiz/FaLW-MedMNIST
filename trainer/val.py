3deebb17ce445683ad50d6de6d05b677fdd178e8
yasiromaiz <yasiromaiz786@gmail.com>
2026-06-03 17:25:25 +0530

    Fix validation metric for chestmnist

--- trainer/val.py
+++ trainer/val.py
@@ -11,7 +11,23 @@ def validate(val_loader, model, criterion, args):
     losses = utils.AverageMeter()
     top1 = utils.AverageMeter()
     device = (torch.device("cuda:%s"%args.gpu) if torch.cuda.is_available() else torch.device("cpu"))
-    accuracy_metric = torchmetrics.Accuracy(task="multiclass", num_classes=args.num_classes+(1 if args.unlearn=='boundary_expanding' else 0), average="none").to(device)
+    
+    # accuracy_metric = torchmetrics.Accuracy(task="multiclass", num_classes=args.num_classes+(1 if args.unlearn=='boundary_expanding' else 0), average="none").to(device)
+    
+    # commenting the above line and adding the below line for chestmnist
+    if args.dataset == "chestmnist":
+        accuracy_metric = None
+    else:
+        accuracy_metric = torchmetrics.Accuracy(
+            task="multiclass",
+            num_classes=args.num_classes + (
+                1 if args.unlearn=='boundary_expanding' else 0
+            ),
+            average="none"
+        ).to(device)
+    
+    
+    
     # switch to evaluate mode
     model.eval()
     if args.imagenet_arch:
@@ -25,8 +41,15 @@ def validate(val_loader, model, criterion, args):
             loss = loss.float()
 
             # measure accuracy and record loss
-            accuracy_metric.update(output,target)
-            
+           
+            # accuracy_metric.update(output,target)
+
+            # commenting above line and added the below line for the chesrtmnist
+            if args.dataset != "chestmnist":
+                accuracy_metric.update(output,target)
+
+
+
             # prec1 = utils.accuracy(output.data, target)[0]
 
             # commenting above line and adding the below line of code for chestmnist
@@ -64,7 +87,13 @@ def validate(val_loader, model, criterion, args):
             loss = loss.float()
 
             # measure accuracy and record loss
-            accuracy_metric.update(output,target)
+
+            # accuracy_metric.update(output,target)
+
+            # commenting the above line and adding the below line for chestmnist
+            if args.dataset != "chestmnist":
+                accuracy_metric.update(output,target)
+
 
             # prec1 = utils.accuracy(output.data, target)[0]
 
@@ -88,6 +117,16 @@ def validate(val_loader, model, criterion, args):
                 )
 
         print("valid_accuracy {top1.avg:.3f}".format(top1=top1))
-    per_class_accuracies = accuracy_metric.compute().cpu().numpy()
+    
+    # per_class_accuracies = accuracy_metric.compute().cpu().numpy()
+    
+    # commenting the above line and adding the below line for chestmnist
+    if args.dataset != "chestmnist":
+        per_class_accuracies = accuracy_metric.compute().cpu().numpy()
+    else:
+        per_class_accuracies = None
+        
+    
+    
     # print("per_class_accuracies:",per_class_accuracies)
     return top1.avg,per_class_accuracies
